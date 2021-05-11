850a5edbdd0c8f38ecd61c3d87edf376ba4d85ed
SunDoge <384813529@qq.com>
2021-05-11 11:26:02 +0800

    没有明显区别

--- cuda_metric_position.py
+++ cuda_metric_position.py
@@ -22,14 +22,21 @@ def bench_normal(model: nn.Module, x: Tensor, n=100):
 
         # Computation
         y = model(x_cuda)
-        y.sum().backward()
-        model.zero_grad()
+        loss = y.sum()
+        loss.backward()
 
         output = (y, target)
 
         # Compute metrics
         acc1, acc5 = accuracy(*output, topk=(1, 5))
         # time.sleep(0.1)
+
+        # Sync
+        loss.item()
+        acc1.item()
+        acc5.item()
+
+        model.zero_grad()
 
         torch.cuda.synchronize()
 
@@ -41,6 +48,7 @@ def bench_history(model: nn.Module, x: Tensor, n=100):
     y1 = model(x1)
     target1 = torch.ones(x1.size(0), device=x1.device)
     output = (y1, target1)
+    loss = torch.tensor(0, device=x1.device)
 
     for _ in trange(n):
         # Transfer data to gpu
@@ -51,14 +59,19 @@ def bench_history(model: nn.Module, x: Tensor, n=100):
         if output is not None:
             acc1, acc5 = accuracy(*output, topk=(1, 5))
             # time.sleep(0.1)
+            loss.item()
+            acc1.item()
+            acc5.item()
+            torch.cuda.synchronize()
 
         # Compute and update history
         y = model(x_cuda)
-        y.sum().backward()
+        loss = y.sum()
+        loss.backward()
         model.zero_grad()
         output = (y, target)
 
-        torch.cuda.synchronize()
+        
 
 
 def main():
